77b7208efaabb7696a09b1f4dc328405dd58e46b
alfystar <alfystar1701@gmail.com>
2020-04-17 22:00:41 +0200

    es5.5

---
+++ 2_OSC2/esempiBook_matlab/es5-5/mainEs.m
@@ -0,0 +1,11 @@
+% Dato un Sistema tempo variante, e sapendo lo stato finale
+% Cercare di minimizzare "L'energia" finale del sistema, mantenendo minima
+% l'energia usata, il tutto in un tempo T dato
+
+% xdot = b(t) * u(t)
+% x(0)=xi
+
+% J(x,u)=a^2 * x^2(T)/2 + Integrale[0,T,u^2(t)dt]
+
+% Per ora senza saturazioni sul valore di u(t)
+
